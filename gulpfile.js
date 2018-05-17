f4b385966dbc2085bac1b07a21d7fa503a40f17a
Shital Bhutiya <ssbhutiya@gmail.com>
2018-05-17 14:27:45 -0400

    transpile the js files from es6 to es5

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let gulp = require('gulp');
 let autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 gulp.task('css',function(){
     gulp.src('src/css/*.css')
     .pipe(autoprefixer({
@@ -15,4 +16,12 @@ gulp.task('images', function(){
     gulp.src('src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('js',function(){
+    gulp.src('./src/js/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist/js/'))
 });
