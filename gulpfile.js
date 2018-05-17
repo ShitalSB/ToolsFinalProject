63fcd3b05e2cba8b18f8df31a47c1cee157273a0
Shital Bhutiya <ssbhutiya@gmail.com>
2018-05-17 14:11:45 -0400

    optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 let gulp = require('gulp');
 let autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 gulp.task('css',function(){
     gulp.src('src/css/*.css')
     .pipe(autoprefixer({
@@ -8,4 +9,10 @@ gulp.task('css',function(){
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('images', function(){
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
 });
